1a9cb9cc7c102acb92a9d4b2065609df96b5a21a
TAN WEI NENG <tanneng@student.usm.my>
2026-01-01 22:30:57 +0800

    bookinfo.java

--- src/main/java/com/novelnest/cat201project/models/BookInfo.java
+++ src/main/java/com/novelnest/cat201project/models/BookInfo.java
@@ -5,13 +5,41 @@ public class BookInfo {
     private String title;
     private String synopsis;
     private double price;
+    private int quantity;
+    private String imagePath;
 
-
-    public BookInfo(int id, String title, String synopsis, double price) {
-        this.id = id;
-        this.title = title;
-        this.synopsis = synopsis;
+    public BookInfo(String imagePath, int quantity, double price, String synopsis, String title, int id) {
+        this.imagePath = imagePath;
+        this.quantity = quantity;
         this.price = price;
+        this.synopsis = synopsis;
+        this.title = title;
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
     }
 
     public double getPrice() {
@@ -22,7 +50,19 @@ public class BookInfo {
         this.price = price;
     }
 
-    public int getId() { return id; }
-    public String getTitle() { return title; }
-    public String getSynopsis() { return synopsis; }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
 }
