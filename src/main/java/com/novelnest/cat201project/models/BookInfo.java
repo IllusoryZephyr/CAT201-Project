7f9ca76d2eeb9e723bd61a9b6fd8bfbdc5c72787
TAN WEI NENG <tanneng@student.usm.my>
2026-01-01 22:27:31 +0800

    bookinfo.java

--- src/main/java/com/novelnest/cat201project/models/BookInfo.java
+++ src/main/java/com/novelnest/cat201project/models/BookInfo.java
@@ -4,11 +4,22 @@ public class BookInfo {
     private int id;
     private String title;
     private String synopsis;
+    private double price;
 
-    public BookInfo(int id, String title, String synopsis) {
+
+    public BookInfo(int id, String title, String synopsis, double price) {
         this.id = id;
         this.title = title;
         this.synopsis = synopsis;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public int getId() { return id; }
