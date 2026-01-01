506fb74ff5aaa18ba93e83f3ab4d0d972765d8ff
TAN WEI NENG <tanneng@student.usm.my>
2026-01-01 20:45:23 +0800

    bookinfo.java

--- src/main/java/com/novelnest/cat201project/models/BookInfo.java
+++ src/main/java/com/novelnest/cat201project/models/BookInfo.java
@@ -1,72 +1,17 @@
 package com.novelnest.cat201project.models;
 
 public class BookInfo {
+    private int id;
     private String title;
-    private double price;
-    private String author;
-    private String imagePath;
-    private int quantity;
-    private String category;
+    private String synopsis;
 
-    public BookInfo(String category, int quantity, String imagePath, String author, double price, String title) {
-        this.category = category;
-        this.quantity = quantity;
-        this.imagePath = imagePath;
-        this.author = author;
-        this.price = price;
+    public BookInfo(int id, String title, String synopsis) {
+        this.id = id;
         this.title = title;
+        this.synopsis = synopsis;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getSynopsis() { return synopsis; }
 }
-
-
-
-
-
