99f27b1377a674b03caaf8ecb3be1fe0f727c512
IllusoryZephyr <illusoryzephyr@gmail.com>
2026-01-09 15:19:50 +0800

    removed old database connection code

--- src/main/java/com/novelnest/cat201project/dao/DatabaseConnection.java
+++ src/main/java/com/novelnest/cat201project/dao/DatabaseConnection.java
@@ -1,36 +1,3 @@
-/*
------------------For MySql------------------
-package com.novelnest.cat201project.dao;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.*;
-
-
-public class DatabaseConnection {
-    public static Connection getConnection() throws SQLException {
-        Connection con = null;
-
-        Dotenv dotenv = Dotenv.load();
-
-        // Ensure these match your .env keys
-        String url = dotenv.get("DB_URL");
-        String user = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASS");
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, user, password);
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("Database Connection Failed!");
-            e.printStackTrace();
-        }
-        return con; // Returns the connection (or null if failed)
-
-    }
-}
-*/
-//--------------For Oracle-----------------------
 package com.novelnest.cat201project.dao;
 
 import io.github.cdimascio.dotenv.Dotenv;
